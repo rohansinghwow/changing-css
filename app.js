97fcb1611cd0ceb4b5bf1ed403aafa20bf82b93a
rohansinghwow <samfrankie439@gmail.com>
2021-03-17 08:38:10 +0530

    Adding index.html styles.css and app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const bodyEl = document.querySelector("body");
+const divEl = document.querySelector("div")
+bodyEl.style.backgroundColor = "grey"
+bodyEl.style.margin = "0"
+bodyEl.style.fontFamily = "sans-serif"
+
+divEl.style.width = "600px"
+divEl.style.margin = "5em auto"
+divEl.style.padding = "2em"
+divEl.style.backgroundColor = "#fdfdff"
+divEl.style.borderRadius = "0.5em"
